6ba2934a298e6b0ff1083a128216550ec8359205
anshurajjjj <114300397+anshurajjjj@users.noreply.github.com>
2022-09-24 22:41:23 +0530

    Add files via upload

---
+++ assingment_1.py
@@ -0,0 +1,38 @@
+#ques_1
+from platform import python_version
+print("Current Python Version-", python_version())
+
+#ques_2
+print('Hello, World!')
+
+#q_3
+#python assingment_1.py
+
+#q_4
+print('Hello, World!')
+
+#q_5
+print("hello\nworld")
+
+#q_6
+print("MySirG")
+
+#q_7
+print("Teacher's Day")
+
+#q_8
+a = 'anshu raj'
+print(a)
+
+#q_9
+name = 'anhu raj'
+year = 20
+qual = 'be'
+exp = 5
+print(name, year, qual, exp)
+
+#q_10
+a = 12
+b = 34
+c = 89
+print(a,b,c, sep = '\n')
