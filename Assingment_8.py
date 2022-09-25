35b04fe25617fafdb7c9b47546b87a5253cc78d6
anshurajjjj <114300397+anshurajjjj@users.noreply.github.com>
2022-09-25 13:30:15 +0530

    Add files via upload

---
+++ Assingment_8.py
@@ -0,0 +1,61 @@
+#Q_1
+a = 'MySirG'
+int = 0 
+while int <= 5:
+    print(a)
+    int = int+1
+
+#Q_2
+i = 1
+while i<=10 :
+    print(i)
+    i = i+1
+
+#Q_3
+i = 10
+while i>=1:
+    print(i)
+    i = i-1
+
+#Q_4
+odd = 1
+while (odd<=10):
+    print(2*odd-1)
+    odd = odd+1
+
+#Q_5
+odd = 19
+while (odd>=10):
+    print(2*odd-1)
+    odd = odd-1
+
+#Q_6
+even = 1
+while (even<=10):
+    print(2*even)
+    even = even +1
+
+#Q_7
+even = 10
+while (even>=1):
+    print(2*even)
+    even = even -1
+
+
+#Q_8
+sqr = 1
+while (sqr<=10):
+    print(sqr*sqr)
+    sqr = sqr+1
+
+#Q_9
+sqr = 1
+while (sqr<=10):
+    print(sqr*sqr*sqr)
+    sqr = sqr+1
+
+#Q_10
+mul = 1
+while (mul<=5):
+    print(mul*5)
+    mul = mul + 1
